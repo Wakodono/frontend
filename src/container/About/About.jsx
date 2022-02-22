ee90ff5d23045ec85e3371f6c2ccf10a9b398f8f
Wakodono <53127265+Wakodono@users.noreply.github.com>
2022-02-22 15:51:32 +0000

    Adds about and connects to backend_sanity

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { motion } from 'framer-motion'
+
+import { images } from '../../constants'
 import './About.scss'
+import { urlFor, client } from '../../client'
 
 const About = () => {
+  const [aboutTopics, setAboutTopics] = useState([])
+
+  useEffect(() => {
+    const query = '*[_type == "aboutTopics"]'
+  
+    client.fetch(query).then(data => setAboutTopics(data))
+  }, [])
+  
   return (
-    <div>About</div>
+    <>
+      <h2 className="head-text">We All Know That <span>Good Code</span> <br /> means <span>Good Business</span>
+      </h2>
+
+      <div className="app__profiles">
+        {
+          aboutTopics.map((topic, index) => (
+            <motion.div
+              whileInView={{ opacity: 1 }}
+              whileHover={{ scale: 1.1 }}
+              transition={{ duration: 0.5, type: 'tween' }}
+              className="app__profile-item"
+              key={topic.title + index}
+            >
+              <img src={topic.imgUrl} alt={topic.title} />
+              <h2 className="bold-text" style={{ marginTop: 20 }}>
+                  {topic.title}
+              </h2>
+              <p className="p-text" style={{ marginTop: 10 }}>
+                  {topic.description}
+              </p>
+            </motion.div>
+          ) )
+        }
+      </div>
+    </>
   )
 }
 
