87009235b8347cbf83c4d52ee4a94fef1dfc7c73
Wakodono <53127265+Wakodono@users.noreply.github.com>
2022-02-22 14:24:18 +0000

    Updates Header

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -60,7 +60,7 @@ const Header = () => {
         className="app__header-circles"
       >
         {
-          [images.flutter, images.redux, images.sass].map((circle, index) => (
+          [images.react, images.typescript, images.sass].map((circle, index) => (
             <div className="circle-cmp app__flex" key={`circle-${index}`}>
               <img src={circle} alt="circle" />
             </div>
