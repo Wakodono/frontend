9ca4a144891ed2240f1fd2d30deb6597221d05f2
Wakodono <53127265+Wakodono@users.noreply.github.com>
2022-02-24 15:35:46 +0000

    Finishes Work (portfolio section)

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -8,7 +8,7 @@ import './Work.scss'
 
 const Work = () => {
   
-  const [Works, setWorks] = useState([])
+  const [works, setWorks] = useState([])
   const [filterWork, setFilterWork] = useState([])
   const [activeFilter, setActiveFilter] = useState('All')
   const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1})
@@ -23,7 +23,20 @@ const Work = () => {
   }, [])
   
 
-  const handleWorkFilter = (item) => {}
+  const handleWorkFilter = (item) => {
+    setActiveFilter(item)
+    setAnimateCard([{ y: 100, opacity: 0 }])
+
+    setTimeout(() => {
+      setAnimateCard([{ y: 0, opacity: 1 }])
+
+      if (item === 'All') {
+        setFilterWork(works)
+      } else {
+        setFilterWork(works.filter((work) => work.tags.includes(item)));
+      }
+    }, 500)
+  }
 
   return (
     <>
