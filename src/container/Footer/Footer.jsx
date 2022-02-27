3b777118f150623e77567ce7cf874f08daf7872c
Wakodono <53127265+Wakodono@users.noreply.github.com>
2022-02-27 20:09:38 +0000

    Updates Footer

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import { images } from '../../constants';
+import { AppWrap, MotionWrap } from "../../wrapper";
+import { urlFor, client } from "../../client";
 import './Footer.scss'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <>
+      <h2 className="head-text">Grab a coffee & chat with me</h2>
+
+      <div className="app__footer-cards">
+        <div className="app__footer-card">
+          <img src={images.email} alt="email" />
+          <a href="mailto:wakoanindo@outlook.com" className="p-text">wakoanindo@outlook.com</a>
+        </div>
+        <div className="app__footer-card">
+          <img src={images.mobile} alt="phone" />
+          <a href="tel:+44 (51) 263-8080" className="p-text">+44 (751) 263-8080</a>
+        </div>
+      </div>
+    </>
   )
 }
 
-export default Footer
+export default AppWrap(
+  MotionWrap(Footer, "app__footer"),
+  "footer",
+  "app__whitebg"
+)
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -29,7 +29,7 @@ const Header = () => {
             <span>👋🏾</span>
             <div style={{ marginLeft: 20}}>
               <p className="p-text">Hello, I am</p>
-              <h1 className="head-text">Micael</h1>
+              <h1 className="head-text">Wako</h1>
             </div>
           </div>
 
